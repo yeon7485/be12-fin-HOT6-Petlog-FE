90d6a8203da5c57eb0143186c91d6f145cea39ba
seiyeon <wsx4379@naver.com>
2025-04-03 16:53:25 +0900

    [Fix] 회원가입/로그인 페이지에서 헤더 안 보이게 설정
    회원가입/로그인 페이지에서 헤더 안 보이게 설정

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+import { useRoute } from "vue-router";
 import Header from "./components/common/Header.vue";
+
+const route = useRoute();
 </script>
 
 <template>
-  <Header></Header>
+  <Header v-if="!route.path.startsWith('/user')" />
   <router-view></router-view>
 </template>
 

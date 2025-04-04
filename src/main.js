1553b576c3881e41ed18dffba5ed71a96c8c8301
seiyeon <wsx4379@naver.com>
2025-04-04 17:58:37 +0900

    [Chore] vuetify 설치
    vuetify 설치

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from "./router";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
 import "./reset.css";
 import "./style.css";
+import "./fullcalendar.css";
 
 const pinia = createPinia();
 
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
+app.use(vuetify);
 
 app.mount("#app");

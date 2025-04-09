f6aa286b6ebf353d787df71988a344596e696174
dong hwi <ehdgnl3465@naver.com>
2025-04-09 20:21:37 +0900

    [Fix] 페이지 명 수정

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 import { RouterView, useRoute } from "vue-router";
 import Header from "./pages/common/components/Header.vue";
 import admin from "./pages/admin/Admin.vue";
+import KakaoMap from "./pages/place/KakaoMap.vue";
 
 const route = useRoute();
 </script>
 
 <template>
   <Header v-if="!route.path.startsWith('/user')" />
-  <router-view></router-view>
+  <RouterView></RouterView>
 </template>
 
 <style scoped></style>

5a6794eb3e222ba760ed5ff5165aee1600ce91a0
seiyeon <wsx4379@naver.com>
2025-04-10 16:18:32 +0900

    [Feat] color format utils 구현
    color format utils 구현

---
+++ src/utils/color.js
@@ -0,0 +1,18 @@
+export function hexToRgba(hex, alpha = 1) {
+  let r = 0,
+    g = 0,
+    b = 0;
+
+  // #fff → #ffffff
+  if (hex.length === 4) {
+    r = parseInt(hex[1] + hex[1], 16);
+    g = parseInt(hex[2] + hex[2], 16);
+    b = parseInt(hex[3] + hex[3], 16);
+  } else if (hex.length === 7) {
+    r = parseInt(hex.slice(1, 3), 16);
+    g = parseInt(hex.slice(3, 5), 16);
+    b = parseInt(hex.slice(5, 7), 16);
+  }
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
